b7aeedc78249f08c757316757b124b7115ebd793
saketh-linux <sakethjakkula642@gmail.com>
2020-02-11 07:20:21 +0530

    working

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,34 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+#cookbook_file '/etc/httpd/conf.d/host.conf' do
+#  source 'test.conf'
+#  action :create
+#end
+
+
+template '/etc/httpd/conf.d/host.conf' do
+  source 'host.conf.erb'
+  variables(
+     host_name: 'test.example.com',
+     source: 'test'
+  )
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables(
+    data: "saying hi from #{node['hostname']}"
+  )
+end
+
+
+service 'httpd' do
+  action [ :restart ]
+end
+
+
+
